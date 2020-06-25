83d1c5257a4ba186c8d1f7b004b129d8acc888e8
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-25 19:00:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,7 +8,14 @@ attr_accessor :name, :grade, :id
   end
 
   def self.create_table
-
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
   end
 
 end
