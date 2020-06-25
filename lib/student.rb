ff1525870f376882865dd2083114ed28feb5a4ce
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-25 19:03:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,7 +25,7 @@ attr_accessor :name, :grade, :id
 
     DB[:conn].execute(sql, self.name, self.grade)
 
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    # @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 
   end
 
